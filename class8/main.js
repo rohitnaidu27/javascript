bef976074e63fbedb5f4fafd2ce70e56e0f5b416
rohitnaidu27 <rohit.chalumuri@yandex.com>
2021-10-13 20:03:13 -0400

    rohit class 8

---
+++ class8/main.js
@@ -0,0 +1,8 @@
+var city = {
+  name: 'djj',
+  location: 92828,
+  popu: 35000
+}
+for(var key in city){
+  console.log("key is"+key +"and the value of it is"+city.+key);
+}
---
+++ class8/script.js
@@ -0,0 +1,3 @@
+var marks = [90, 91, 92, 94];
+console.log(marks.length);
+console.log(marks[0]);
